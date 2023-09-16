2cd8816bf70dcef814e200716a84ea7947814634
steven nevins <steven.nevins.js@gmail.com>
2023-09-15 22:54:12 -0400

    minimal proxy

--- test/Create2.t.sol
+++ test/Create2.t.sol
@@ -25,4 +25,20 @@ contract Create2Test is Test {
 
         Emitter(predictedTo).ping();
     }
+
+    function test_Create2MinimalProxy() public {
+        Emitter emitter = new Emitter();
+        address implementation = address(emitter);
+        bytes memory creationCode = bytes.concat(
+            hex"3d602d80600a3d3981f3363d3d373d3d3d363d73", bytes20(implementation), hex"5af43d82803e903d91602b57fd5bf3"
+        );
+        bytes32 salt = bytes32(0);
+        bytes memory initCode = bytes.concat(creationCode, abi.encode());
+
+        (bool success,) = CREATE2_FACTORY.call(bytes.concat(salt, initCode));
+        assertTrue(success);
+        address predictedTo = computeCreate2Address(salt, hashInitCode(creationCode));
+
+        Emitter(predictedTo).ping();
+    }
 }
