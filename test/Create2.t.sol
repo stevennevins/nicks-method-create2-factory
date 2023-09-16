93517e5c88f6b9d14134aa95984a39cf875497aa
steven nevins <steven.nevins.js@gmail.com>
2023-09-16 12:28:56 -0400

    add gas check

--- test/Create2.t.sol
+++ test/Create2.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
+import Test} from "forge-std/Test.sol";
 
 contract Emitter {
     event Ping();
@@ -40,5 +40,18 @@ contract Create2Test is Test {
         address predictedTo = computeCreate2Address(salt, hashInitCode(creationCode));
 
         Emitter(predictedTo).ping();
+    }
+
+    function testGas_Create2MinimalProxy() public {
+        vm.pauseGasMetering();
+        address implementation;
+        bytes memory creationCode = bytes.concat(
+            hex"3d602d80600a3d3981f3363d3d373d3d3d363d73", bytes20(implementation), hex"5af43d82803e903d91602b57fd5bf3"
+        );
+        bytes32 salt;
+        bytes memory initCode = bytes.concat(creationCode, abi.encode());
+
+        vm.resumeGasMetering();
+        (bool success,) = CREATE2_FACTORY.call(bytes.concat(salt, initCode));
     }
 }
